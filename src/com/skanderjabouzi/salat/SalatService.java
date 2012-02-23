b45db3677544d85d719facd00f8ff8b7faa1335b
Skander Jabouzi <jabouzi@gmail.com>
2012-02-23 16:10:14 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/salatAndroid/trunk@23 86cb5e81-20db-4be9-97d3-054b39bd930a

--- src/com/skanderjabouzi/salat/SalatService.java
+++ src/com/skanderjabouzi/salat/SalatService.java
@@ -4,7 +4,7 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.os.Vibrator;
+//import android.os.Vibrator;
 import android.content.Intent;
 import android.util.Log;
 import java.util.Calendar;
@@ -37,7 +37,7 @@ public class SalatService extends IntentService {
         int mSeconds = date.getSeconds();       
         this.notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE); 
         this.notification = new Notification(R.drawable.makka_icon,"", 0);         
-        this.notification.defaults |= Notification.DEFAULT_VIBRATE;
+        //this.notification.defaults |= Notification.DEFAULT_VIBRATE;
         Log.d(TAG, "onHandleIntent #3 " + mHour + "  " + mMinute+ "  " + mSeconds);
         String salatName = salatApp.getNextSalat();
         /*long[] vibrate = {0,100,200,300};
@@ -61,7 +61,7 @@ public class SalatService extends IntentService {
         this.notification.ledARGB = 0xff00ff00;
         this.notification.ledOnMS = 300;
         this.notification.ledOffMS = 1000;
-        this.notification.vibrate = new long[]{0,100,200,300};
+        //this.notification.vibrate = new long[]{0,100,200,300};
         CharSequence notificationTitle = this.getText(R.string.msgNotificationTitle); 
         CharSequence notificationSummary = this.getString(R.string.msgNotificationMessage, salatName);
         this.notification.setLatestEventInfo(this, notificationTitle, notificationSummary, pendingIntent); 
