ebe8a71047965b4bc2e933b9e0e0dd21c71a5153
Skander Jabouzi <jabouzi@gmail.com>
2012-03-12 13:03:03 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/salatAndroid/trunk@54 86cb5e81-20db-4be9-97d3-054b39bd930a

---
+++ src/com/skanderjabouzi/salat/SalatReceiver.java
@@ -0,0 +1,18 @@
+package com.skanderjabouzi.salat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class SalatReceiver extends BroadcastReceiver {     
+
+  @Override
+  public void onReceive(Context context, Intent intent) { 
+    WakeLock.acquire(context);    
+    //SalatApplication salatApp = (SalatApplication) getApplication(); 
+    //this.startAthan(context);
+    context.startService(new Intent(context, AthanService.class)); 
+    Log.d("SalatReceiver", "onReceived");
+  }
+}
