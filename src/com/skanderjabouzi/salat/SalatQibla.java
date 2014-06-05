3e3fa1305aaab87635b71b7558b159cc8d2c1b50
Skander Jabouzi <jabouzi@gmail.com>
2014-06-05 01:40:12 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/salatAndroid/trunk@195 86cb5e81-20db-4be9-97d3-054b39bd930a

--- src/com/skanderjabouzi/salat/SalatQibla.java
+++ src/com/skanderjabouzi/salat/SalatQibla.java
@@ -1,59 +1,103 @@
 package com.skanderjabouzi.salat;
 
 import android.app.Activity;
-import android.hardware.SensorListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-// implement SensorListener
-public class SalatQibla extends Activity implements SensorListener {
-  SensorManager sensorManager;
-  static final int sensor = SensorManager.SENSOR_ORIENTATION;
-  Rose rose;
+public class SalatQibla extends Activity implements SensorEventListener {
 
-  /** Called when the activity is first created. */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	// define the display assembly compass picture
+	private ImageView image;
+	private ImageView image2;
 
-    // Set full screen view
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	// record the compass picture angle turned
+	private float currentDegree = 0f;
 
-    rose = new Rose(this);
+	// device sensor manager
+	private SensorManager mSensorManager;
 
-    setContentView(rose);
+	TextView textDegree;
 
-    // get sensor manager
-    sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.qibla);
+		image2 = (ImageView) findViewById(R.id.compass2);
+		rotate(image2, 58.64f);
+		// our compass image
+		image = (ImageView) findViewById(R.id.compass3);
 
-  // register to listen to sensors
-  @Override
-  public void onResume() {
-    super.onResume();
-    sensorManager.registerListener(this, sensor);
-  }
+		// TextView that will tell the user what degree is he heading
+		textDegree = (TextView) findViewById(R.id.degree);
 
-  // unregister
-  @Override
-  public void onPause() {
-    super.onPause();
-    sensorManager.unregisterListener(this);
-  }
+		// initialize your android device sensor capabilities
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+	}
 
-  // Ignore for now
-  public void onAccuracyChanged(int sensor, int accuracy) {
-  }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		// for the system's orientation sensor registered listeners
+		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+				SensorManager.SENSOR_DELAY_GAME);
+	}
 
-  // Listen to sensor and provide output
-  public void onSensorChanged(int sensor, float[] values) {
-    if (sensor != SalatQibla.sensor)
-      return;
-    int orientation = (int) values[0];
-    rose.setDirection(orientation);
-  }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		// to stop the listener and save battery
+		mSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+
+		// get the angle around the z-axis rotated
+		float degree = Math.round(event.values[0]);
+
+		textDegree.setText(Float.toString(degree));
+
+		// create a rotation animation (reverse turn degree degrees)
+		RotateAnimation ra = new RotateAnimation(
+				currentDegree, 
+				-degree,
+				Animation.RELATIVE_TO_SELF, 0.5f, 
+				Animation.RELATIVE_TO_SELF,
+				0.5f);
+
+		// how long the animation will take place
+		ra.setDuration(210);
+
+		// set the animation after the end of the reservation status
+		ra.setFillAfter(true);
+
+		// Start the animation
+		image.startAnimation(ra);
+		currentDegree = -degree;
+
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// not in use
+	}
+	
+	private void rotate(ImageView imgview, float degree) {
+		RotateAnimation rotateAnim = new RotateAnimation(0.0f, -degree,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+				RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+		rotateAnim.setDuration(0);
+		rotateAnim.setFillAfter(true);
+		imgview.startAnimation(rotateAnim);
+	}
 }
