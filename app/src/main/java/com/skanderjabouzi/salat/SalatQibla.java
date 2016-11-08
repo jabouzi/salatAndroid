716fec393b3e85f8141f654200ddede4f6c6c8e9
Skander Jabouzi <skander.jabouzi@geemedia.com>
2016-11-08 09:38:52 -0500

    clean qibla code

--- app/src/main/java/com/skanderjabouzi/salat/SalatQibla.java
+++ app/src/main/java/com/skanderjabouzi/salat/SalatQibla.java
@@ -17,12 +17,6 @@ import android.text.Html;
 import android.util.Log;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Paint;
-import android.graphics.Matrix;
-import android.graphics.RectF;
-import android.graphics.Canvas;
 import android.animation.ObjectAnimator;
 import java.util.ArrayList;
 import android.animation.AnimatorSet;
@@ -34,7 +28,6 @@ public class SalatQibla extends Activity implements SensorEventListener {
 	private ImageView image2;
 	private int sensorAccuracy;
 	private float currentDegree = 0f;
-	//private float currentDegree2 = 178f;
 	private SensorManager mSensorManager;
 	TextView compassDegree;
 	TextView compassDegreeTitle;
@@ -48,20 +41,15 @@ public class SalatQibla extends Activity implements SensorEventListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//startService(new Intent(context, LocationService.class));
 		setContentView(R.layout.qibla);
 		qiblaLayout = findViewById(R.id.qibla_bg);
 		image = (ImageView) findViewById(R.id.compass);
 		image2 = (ImageView) findViewById(R.id.compass2);
-        image2.setImageBitmap(rotateImage());
-//		image2 = (ImageView) findViewById(R.id.compass2);
-		//rotate(image2, 0, 178f, 0);
 		compassDegree = (TextView) findViewById(R.id.degree);
 		compassDegreeTitle = (TextView) findViewById(R.id.degree_title);
 		qiblaDegree = (TextView) findViewById(R.id.qibla_degree);
 		qiblaDegreeTitle = (TextView) findViewById(R.id.qibla_degree_title);
-		//qiblaDegree.setText(Float.toString(58.64f));
-		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);		
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		datasource = new LocationDataSource(this);
 		datasource.open();
 		location = datasource.getLocation(1);
@@ -79,7 +67,6 @@ public class SalatQibla extends Activity implements SensorEventListener {
 	@Override
     protected void onPause() {
         super.onPause();
-        //finish();
         mSensorManager.unregisterListener(this);
     }
     
@@ -87,23 +74,19 @@ public class SalatQibla extends Activity implements SensorEventListener {
     protected void onStop() {
         super.onStop();
         if (datasource.isOpen()) datasource.close();
-        //finish();
-        //mSensorManager.unregisterListener(this);
     }
     
 	@Override
     protected void onDestroy() {
         super.onDestroy();
         if (datasource.isOpen()) datasource.close();
-        //finish();
-        //mSensorManager.unregisterListener(this);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
     @Override
 	public void onSensorChanged(SensorEvent event) {
 		float degree = Math.round(event.values[0]);
-		
+
 		if ((int)degree == (int)getQibla())
 		{
 			background_changed = true;
@@ -119,7 +102,7 @@ public class SalatQibla extends Activity implements SensorEventListener {
 		}
 		
 		compassDegree.setText(String.format("%d",(int)degree));
-		rotate(image, currentDegree, degree, 300);
+		rotate(image, currentDegree, degree, getQibla(), 300);
 		currentDegree = -degree;
 	}
 
@@ -130,35 +113,26 @@ public class SalatQibla extends Activity implements SensorEventListener {
 	}
 
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
-    private void rotate(ImageView imgview, float currentDegree, float degree, int duration) {
+    private void rotate(ImageView imgview, float currentDegree, float degree, float qibla, int duration) {
 
 		ArrayList<ObjectAnimator> arrayListObjectAnimators = new ArrayList<ObjectAnimator>();
         ObjectAnimator anim1 = ObjectAnimator.ofFloat(image , "rotation", currentDegree, -degree);
         arrayListObjectAnimators.add(anim1);
-        ObjectAnimator anim2 = ObjectAnimator.ofFloat(image2 , "rotation", currentDegree, -degree);
+        ObjectAnimator anim2 = ObjectAnimator.ofFloat(image2 , "rotation", currentDegree + qibla, -degree + qibla);
         arrayListObjectAnimators.add(anim2);
 
         ObjectAnimator[] objectAnimators = arrayListObjectAnimators.toArray(new ObjectAnimator[arrayListObjectAnimators.size()]);
         AnimatorSet animSetXY = new AnimatorSet();
         animSetXY.playTogether(objectAnimators);
-        animSetXY.setDuration((long) 0.5f);//1sec
+        animSetXY.setDuration((long) duration);
         animSetXY.start();
-//		RotateAnimation rotateAnim = new RotateAnimation(currentDegree, -degree,
-//				RotateAnimation.RELATIVE_TO_SELF, 0.5f,
-//				RotateAnimation.RELATIVE_TO_SELF, 0.5f);
-//
-//		rotateAnim.setDuration(duration);
-//		rotateAnim.setFillAfter(true);
-//		imgview.startAnimation(rotateAnim);
 	}
 
 	private float getQibla()
 	{    
 		final float MLONG = 39.823333f;
 		final float MLAT = 21.42333f;    
-		//final float Math.PI = 4.0f*Math.atan(1.0f);
 
-        Log.d("LNG : ", String.valueOf(location.getLongitude()));
 		float x1 = (float)Math.sin((-location.getLongitude()+MLONG)*Math.PI/180f);
 		float y1 = (float)Math.cos(location.getLatitude()*Math.PI/180f) * (float)Math.tan(MLAT*Math.PI/180f);
 		float y2 = (float)Math.sin(location.getLatitude()*Math.PI/180f) * (float)Math.cos((-location.getLongitude()+MLONG)*Math.PI/180f);
@@ -170,45 +144,5 @@ public class SalatQibla extends Activity implements SensorEventListener {
 		}
 		if (qibla_angle > 360f) qibla_angle = qibla_angle - 360f;    
 		return qibla_angle;        
-	}
-
-	private Bitmap initImage()
-	{
-		Bitmap bmpOriginal = BitmapFactory.decodeResource(this.getResources(), R.drawable.arrow);
-		Bitmap targetBitmap = Bitmap.createBitmap((bmpOriginal.getWidth()),	(bmpOriginal.getHeight()), Bitmap.Config.ARGB_8888);
-		Paint p = new Paint();
-		p.setAntiAlias(true);
-		p.setDither(true);
-		p.setFilterBitmap(true);
-
-		Matrix matrix = new Matrix();
-		matrix.setRotate((float) getQibla(),(float) (bmpOriginal.getWidth()/2),
-				(float)(bmpOriginal.getHeight()/2));
-
-		RectF rectF = new RectF(0, 0, bmpOriginal.getWidth(), bmpOriginal.getHeight());
-		matrix.mapRect(rectF);
-
-		targetBitmap = Bitmap.createBitmap((int)rectF.width(), (int)rectF.height(), Bitmap.Config.ARGB_8888);
-
-
-		Canvas tempCanvas = new Canvas(targetBitmap);
-		tempCanvas.drawBitmap(bmpOriginal, matrix, p);
-
-        Bitmap bmOverlay = Bitmap.createBitmap(targetBitmap.getWidth(), targetBitmap.getHeight(), targetBitmap.getConfig());
-        Canvas canvas = new Canvas(bmOverlay);
-        canvas.drawBitmap(targetBitmap, new Matrix(), null);
-        Bitmap bmp2 = BitmapFactory.decodeResource(this.getResources(), R.drawable.cadran);
-        canvas.drawBitmap(bmp2, 0, 0, null);
-
-        return bmOverlay;
-	}
-
-	private Bitmap rotateImage()
-	{
-		Bitmap bmpOriginal = BitmapFactory.decodeResource(this.getResources(), R.drawable.arrow);
-		Matrix matrix = new Matrix();
-		matrix.postRotate(getQibla());
-		Bitmap bitmap = Bitmap.createBitmap(bmpOriginal, 0, 0, bmpOriginal.getWidth(), bmpOriginal.getHeight(),matrix,true);
-		return bitmap;
 	}
 }
